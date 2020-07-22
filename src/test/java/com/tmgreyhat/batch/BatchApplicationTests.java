f87e6682af2086c3d1faf8512ba95a9663ab3abd
Tapiwanashe <tapiwambizvo@gmail.com>
2020-07-22 15:04:20 +0200

    Added all default files

---
+++ src/test/java/com/tmgreyhat/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmgreyhat.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
